d810d0dce1141ada64f46754c7250aea755eb620
Developer <maitryweblabs@gmail.com>
2025-01-18 12:47:38 +0530

    changed api key

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const button = document.querySelector('.button-33');
 const display = document.querySelector('.text');
 let apiEndPoint = 'https://api.opencagedata.com/geocode/v1/json';
-let key = '4d685ba4b46f4903b564fd309c090e02';
+let key = '19c86ced1b8d490e91527b0bfe01fef8';
 
 const getUserCurrentDetails = async (latitude,longitude) => {
     // console.log(latitude,longitude);
     let query = `${latitude},${longitude}`;
     let apiUrl = `${apiEndPoint}?key=${key}&q=${query}&pretty=1`;
     try {
-        let res = await fetch(`https://api.opencagedata.com/geocode/v1/json?key=4d685ba4b46f4903b564fd309c090e02&q=${query}&pretty=1`);
+        let res = await fetch(`https://api.opencagedata.com/geocode/v1/json?key=19c86ced1b8d490e91527b0bfe01fef8&q=${query}&pretty=1`);
         let data = await res.json();
         let address = data.results[0].formatted;
         // console.log();
